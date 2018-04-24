1be4b76e7346d92f547532ee7f307e0c0c128e6f
852Kerfunkle <yvescolle@gmail.com>
2018-04-24 03:46:05 +0200

    proprietary blobs

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,13 +1,21 @@
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc
-
-    #$(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
-    $(LOCAL_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
-    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
-    
+	$(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+	$(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+	$(LOCAL_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
+	$(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
+	$(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+	$(LOCAL_PATH)/rootdir/init.common_svc.rc:root/init.common_svc.rc \
+	$(LOCAL_PATH)/rootdir/init.environ.rc:root/init.environ.rc \
+	$(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+	$(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+	$(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+	$(LOCAL_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
+	$(LOCAL_PATH)/rootdir/init.trace.rc:root/init.trace.rc \
+	$(LOCAL_PATH)/rootdir/init.usb.rc:root/init.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.xlog.rc:root/init.xlog.rc \
+	$(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+	$(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+	$(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+	$(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc
