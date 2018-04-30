13d639a749eae92808bd74a84ab9f6d618b89a25
852Kerfunkle <yvescolle@gmail.com>
2018-04-30 05:43:54 +0200

    * added stack protector pathc the reverts to use of -fstack-protector instead of -fstack-protector to fix camera hal crashing mediaserver * added a few other patches
    Change-Id: I53cefe79ada787c7ff45f027a1474be5b5ee7092

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av packages/apps/Settings system/core"
+dirs="bionic build frameworks/av packages/apps/Settings system/core"
 
 for dir in $dirs ; do
 	cd $rootdirectory
