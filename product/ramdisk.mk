98889c263d8176f44392e977137ffb1241f387df
852Kerfunkle <yvescolle@gmail.com>
2018-05-01 23:52:06 +0200

    allowed fsymc syscall for mediacodec
    Change-Id: Ib3ce712fae270c5c0ccad851a34935a4d26ce10f

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -6,5 +6,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
 	$(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
 	$(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-	$(LOCAL_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc
+	$(LOCAL_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
+	$(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh
 
